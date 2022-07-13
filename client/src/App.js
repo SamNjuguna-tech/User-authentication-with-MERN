b262620797b87e52fee3743367ec2b3fc5de1372
SamNjuguna-tech <snjuguna898@gmail.com>
2022-07-14 00:32:38 +0300

    crete the form

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,30 @@
 import React from 'react'
-import logo from './logo.svg';
-import './App.css';
+import './styles/login.css';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.sam
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <section className='login-parent-container'>
+      <div className='login-system'>
+      <div class="inner-section section1">
+				<h1>MALAIKA INITIATIVE</h1>
+        <p>Love, care and kindness to them that need it the most</p>
+			</div>
+      <div className='inner-section section2'>
+        <form className='logIn-form' id='logIn-form' autoComplete='off'>
+        <span class="title">Log In</span>
+					<label class="label" for="userName">Username</label>
+          <input type="text" id="usn" name="userName" required  autocomplete="off"/>
+					<label class="label" for="userName">password</label>
+          <input type="password" id="psw" name="userName" required/>
+					<p style={{color:"red", display:"none"}} class="alert" id="input-error"></p>
+					{/* <a href="#" class="register">Register</a> */}
+					<input type="submit" value="Login" name="submit" class="submit-button" id="submit-button"/>
+        </form>
+      </div>
+      </div>
+    </section>
+    )
 }
 
 export default App;
